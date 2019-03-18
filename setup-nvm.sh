3e90147181461e9a495b3dec5476ac56803fa597
Rafał Lewandowski <rafallewandowski@Rafas-MacBook-Pro.local>
2019-03-18 14:04:19 +0100

    Fix NVM setup script

--- setup-nvm.sh
+++ setup-nvm.sh
@@ -4,7 +4,7 @@ if ! grep "nvm" ~/.bash_tools > /dev/null 2>&1
 then
   echo '' >> ~/.bash_tools
   echo '# NVM config' >> ~/.bash_tools
-  echo 'export NVM_DIR="$HOME/.nvm' >> ~/.bash_tools
+  echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_tools
   echo '[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm' >> ~/.bash_tools
   echo '[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion' >> ~/.bash_tools
 
