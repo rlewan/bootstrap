e4bbba1626d09e1509ed6b36d240642bcfcb8f7a
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2021-06-11 22:56:49 +0200

    Delete unused files

--- set-dark-mode.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CURRENT_THEME="$(defaults read "Apple Global Domain" "AppleInterfaceStyle" 2>/dev/null)"
-
-if [[ "$CURRENT_THEME" != "Dark" ]]
-then
-  osascript -e "tell application \"System Events\" to tell appearance preferences to set dark mode to true"
-  echo "Enabled dark mode"
-fi
