98c6336273d55a5d885be6b5d4f75382fa0a5c19
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2023-01-06 20:00:38 +0100

    Fix the NVM setup
    It was using pre-apple silicon homebrew paths.

--- setup-nvm.sh
+++ setup-nvm.sh
@@ -5,8 +5,8 @@ then
   echo '' >> ~/.bash_tools
   echo '# NVM config' >> ~/.bash_tools
   echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_tools
-  echo '[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm' >> ~/.bash_tools
-  echo '[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion' >> ~/.bash_tools
+  echo '[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm' >> ~/.bash_tools
+  echo '[ -s "/opt/homebrew/opt/nvm/etc/bash_completion" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion' >> ~/.bash_tools
 
   echo 'NVM configured'
 fi
