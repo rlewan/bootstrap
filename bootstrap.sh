46c6aab473a27f825d63e445b363c603548ef5cf
Rafał Lewandowski <rafal.lewandowski@allegro.com>
2026-09-09 10:17:07 +0200

    Check for Homebrew before proceeding

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,16 @@ function log-single {
   echo -en "${COLOUR}${1}${CLEAR}"
 }
 
+# Check if Homebrew is available
+log-single 'Checking for Homebrew... '
+if [ -n "$(command -v brew)" ]
+then
+  log 'Homebrew is available'
+else
+  log 'Homebrew is not available, install it before continuing'
+  exit 1
+fi
+
 # Check if Ansible is available and install if not
 log-single 'Checking for Ansible... '
 if [ -n "$(command -v ansible-playbook)" ]
